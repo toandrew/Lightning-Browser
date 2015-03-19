421424e7c96f0dad164e494130363d9218c0646e
Jianmin Zhou <toandrew@infthink.com>
2015-03-19 15:03:31 +0800

    Add floating window control and service for 3rd apps.
    Signed-off-by: Jianmin Zhou <toandrew@infthink.com>

--- src/acr/browser/lightning/MainActivity.java
+++ src/acr/browser/lightning/MainActivity.java
@@ -11,87 +11,95 @@ import android.widget.Toast;
 
 public class MainActivity extends BrowserActivity {
 
-	SharedPreferences mPreferences;
+    SharedPreferences mPreferences;
 
-	CookieManager mCookieManager;
+    CookieManager mCookieManager;
 
-	Toast mToast;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mPreferences = getSharedPreferences(PreferenceConstants.PREFERENCES, 0);
-	}
+    Toast mToast;
 
-	@Override
-	public void updateCookiePreference() {
-		if (mPreferences == null) {
-			mPreferences = getSharedPreferences(PreferenceConstants.PREFERENCES, 0);
-		}
-		mCookieManager = CookieManager.getInstance();
-		CookieSyncManager.createInstance(this);
-		mCookieManager.setAcceptCookie(mPreferences.getBoolean(PreferenceConstants.COOKIES, true));
-		super.updateCookiePreference();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPreferences = getSharedPreferences(PreferenceConstants.PREFERENCES, 0);
 
-	@Override
-	public synchronized void initializeTabs() {
-		super.initializeTabs();
-		restoreOrNewTab();
-		// if incognito mode use newTab(null, true); instead
-	}
+//        if (!FxService.isRunning()) {
+//            Intent flingIntent = new Intent(this, FxService.class);
+//            startService(flingIntent);
+//        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    }
 
-	@Override
-	protected void onNewIntent(Intent intent) {
-		handleNewIntent(intent);
-		super.onNewIntent(intent);
-	}
+    @Override
+    public void updateCookiePreference() {
+        if (mPreferences == null) {
+            mPreferences = getSharedPreferences(
+                    PreferenceConstants.PREFERENCES, 0);
+        }
+        mCookieManager = CookieManager.getInstance();
+        CookieSyncManager.createInstance(this);
+        mCookieManager.setAcceptCookie(mPreferences.getBoolean(
+                PreferenceConstants.COOKIES, true));
+        super.updateCookiePreference();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		saveOpenTabs();
-	}
+    @Override
+    public synchronized void initializeTabs() {
+        super.initializeTabs();
+        restoreOrNewTab();
+        // if incognito mode use newTab(null, true); instead
+    }
 
-	@Override
-	public void updateHistory(String title, String url) {
-		super.updateHistory(title, url);
-		addItemToHistory(title, url);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean isIncognito() {
-		return false;
-	}
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleNewIntent(intent);
+        super.onNewIntent(intent);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveOpenTabs();
+    }
+
+    @Override
+    public void updateHistory(String title, String url) {
+        super.updateHistory(title, url);
+        addItemToHistory(title, url);
+    }
+
+    @Override
+    public boolean isIncognito() {
+        return false;
+    }
 
     long preQuitTime = 0;
-    
-	@Override
-	public void closeActivity() {
+
+    @Override
+    public void closeActivity() {
         Log.e("MainActivity", "closeActivity!");
         long currentTime = System.currentTimeMillis();
         if (preQuitTime == 0 || Math.abs(currentTime - preQuitTime) > 1000) {
             preQuitTime = System.currentTimeMillis();
             Log.e("MainActivity", "confirm quit?");
             if (mToast == null) {
-                mToast = Toast.makeText(getApplicationContext(), "Press again to Quit!", Toast.LENGTH_SHORT);
+                mToast = Toast.makeText(getApplicationContext(),
+                        "Press again to Quit!", Toast.LENGTH_SHORT);
             }
             mToast.show();
-            
+
             return;
         }
-       
-		closeDrawers();
-	    //moveTaskToBack(true);
-		
-		// finish it?
-		finish();
-	}
+
+        closeDrawers();
+        // moveTaskToBack(true);
+
+        // finish it?
+        finish();
+    }
 }
