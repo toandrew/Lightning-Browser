d4cec2cf183a43a1e9d5d54d86cb52b48eec6042
Jianmin Zhou <toandrew@infthink.com>
2015-03-30 17:42:46 +0800

    reset media route button status when application disconnected.
    Signed-off-by: Jianmin Zhou <toandrew@infthink.com>

--- src/com/crosskr/flint/browser/BrowserActivity.java
+++ src/com/crosskr/flint/browser/BrowserActivity.java
@@ -3272,6 +3272,11 @@ public class BrowserActivity extends FragmentActivity implements
             detachMediaPlayer();
             clearMediaState();
             updateButtonStates();
+            
+            if (mMediaRouter != null) {
+                mMediaRouter.selectRoute(mMediaRouter.getDefaultRoute());
+            }
+            
             // if (statusCode != FlintStatusCodes.SUCCESS) {
             // This is an unexpected disconnect.
             // setApplicationStatus(getString(R.string.status_app_disconnected));
