a5ba96bbaa1ef84193a79dab437f2b46a9b8a506
Jianmin Zhou <toandrew@infthink.com>
2015-06-08 14:33:38 +0800

    fix dex 65536 issue
    use ant to build the app.
    
    Signed-off-by: Jianmin Zhou <toandrew@infthink.com>

--- src/com/crosskr/flint/webview/browser/BrowserApp.java
+++ src/com/crosskr/flint/webview/browser/BrowserApp.java
@@ -1,9 +1,18 @@
 package com.crosskr.flint.webview.browser;
 
-import com.connectsdk.discovery.DiscoveryManager;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
+
+import com.connectsdk.discovery.DiscoveryManager;
+
+import dalvik.system.DexClassLoader;
 
 public class BrowserApp extends Application {
 
@@ -12,8 +21,11 @@ public class BrowserApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        dexTool();
+
         context = getApplicationContext();
-        
+
         DiscoveryManager.init(getApplicationContext());
     }
 
@@ -56,5 +68,55 @@ public class BrowserApp extends Application {
 
     public void setAppname(String appname) {
         this.appname = appname;
+    }
+
+    /**
+     * Copy the following code and call dexTool() after super.onCreate() in
+     * Application.onCreate()
+     * <p>
+     * This method hacks the default PathClassLoader and load the secondary dex
+     * file as it's parent.
+     */
+    private void dexTool() {
+
+        File dexDir = new File(getFilesDir(), "dlibs");
+        dexDir.mkdir();
+        File dexFile = new File(dexDir, "libs.apk");
+        File dexOpt = new File(dexDir, "opt");
+        dexOpt.mkdir();
+        try {
+            InputStream ins = getAssets().open("libs.apk");
+            if (dexFile.length() != ins.available()) {
+                FileOutputStream fos = new FileOutputStream(dexFile);
+                byte[] buf = new byte[4096];
+                int l;
+                while ((l = ins.read(buf)) != -1) {
+                    fos.write(buf, 0, l);
+                }
+                fos.close();
+            }
+            ins.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ClassLoader cl = getClassLoader();
+        ApplicationInfo ai = getApplicationInfo();
+        String nativeLibraryDir = null;
+        if (Build.VERSION.SDK_INT > 8) {
+            nativeLibraryDir = ai.nativeLibraryDir;
+        } else {
+            nativeLibraryDir = "/data/data/" + ai.packageName + "/lib/";
+        }
+        DexClassLoader dcl = new DexClassLoader(dexFile.getAbsolutePath(),
+                dexOpt.getAbsolutePath(), nativeLibraryDir, cl.getParent());
+
+        try {
+            Field f = ClassLoader.class.getDeclaredField("parent");
+            f.setAccessible(true);
+            f.set(cl, dcl);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
