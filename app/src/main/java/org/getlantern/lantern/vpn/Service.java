f1bcefc11b71324c273d6a240ea953d001cbc655
Jianmin Zhou <jianmin.zhou@acadine.com>
2016-04-11 15:20:49 +0800

    remove useless items(gms etc.)
    Signed-off-by: Jianmin Zhou <jianmin.zhou@acadine.com>

--- app/src/main/java/org/getlantern/lantern/vpn/Service.java
+++ app/src/main/java/org/getlantern/lantern/vpn/Service.java
@@ -60,7 +60,8 @@ public class Service extends VpnBuilder implements Runnable {
                 }
             });
             int startTimeoutMillis = 60000;
-            String analyticsTrackingID = "UA-21815217-14";
+            //String analyticsTrackingID = "UA-21815217-14";
+            String analyticsTrackingID = null; // does not use gms to track info.
             org.lantern.mobilesdk.StartResult result = org.lantern.mobilesdk.Lantern.enable(getApplicationContext(), startTimeoutMillis, analyticsTrackingID);
             configure(result.getSOCKS5Addr());
 
