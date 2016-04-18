ab77825ea02e3b4ab4e6726b5b2bc7c52b9f0759
Jianmin Zhou <jianmin.zhou@acadine.com>
2016-04-18 13:01:20 +0800

    1. Add VPN service connect/disconnect hints. 2. Change version to 1.0.1
    Signed-off-by: Jianmin Zhou <jianmin.zhou@acadine.com>

---
+++ app/src/LightningPlus/java/acr/browser/lightning/activity/AdManager.java
@@ -0,0 +1,16 @@
+package acr.browser.lightning.activity;
+
+import android.content.Context;
+
+public class AdManager {
+    public AdManager(Context context) {
+    }
+
+    public void onCreate() {
+        // TODO
+    }
+
+    public void onDestroy() {
+        // TODO
+    }
+}
